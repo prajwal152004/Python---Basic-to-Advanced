7c30d969ce56bab76ec0241e9b44f4cf5dc10133
Prajwal DM <149014104+prajwal152004@users.noreply.github.com>
2026-07-26 01:18:34 +0530

    Add files via upload

---
+++ 11. Simple Greeting Program.py
@@ -0,0 +1,6 @@
+#Simple Greeting Program: Write a Python program that asks the user for their name and age, then prints a personalized greeting message. Use both the + operator and f-strings for output.
+
+name=input("Enter Your Name: ")
+age= input("Enter Your Age: ")
+print(f"Hello {name}. You're {age}. Welcome to this program")
+print("Welcome once again " +name+".")
