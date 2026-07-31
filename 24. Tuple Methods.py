d30bc6c98c79e53d41e06b623695a32d1889524e
Prajwal DM <149014104+prajwal152004@users.noreply.github.com>
2026-07-31 16:04:12 +0530

    Add files via upload

---
+++ 24. Tuple Methods.py
@@ -0,0 +1,14 @@
+tuple=(1,2,3,4,2)
+tuple_list=("apple","chikoo","banana","banana","custurdapple")
+
+# count() - Returns the number of times an element can appear in a tuple 
+
+print(tuple.count(2))
+print(tuple_list.count("banana"))
+print(tuple)
+
+# index() - Returns the index of first occurance of an element
+
+print(tuple_list.index("apple"))
+print(tuple_list.index("banana"))
+print(tuple.index(3))
