48e4fcabcd4493b316c29f3599b06c427442ec53
Prajwal DM <149014104+prajwal152004@users.noreply.github.com>
2026-07-31 16:03:08 +0530

    Add files via upload

---
+++ 20. List Functions.py
@@ -0,0 +1,16 @@
+#Length of lists - Returns the number of elements in a list
+#Syntax = len(list_name)
+
+list=[5,3,2,4,1]
+print(list)
+print(len(list)) #Output should return the number of elements in a list
+
+#Sorted List - Returns a new sorted list without changing the original list
+#Syntax = sorted(list_name)
+
+print(sorted(list))
+print(list) #the list stays original even after sorting
+
+print(sum(list)) #adds all the numbers in the list and returns the answer
+
+
